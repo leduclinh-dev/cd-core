62837f64507273613cf9f554c08abf7dd8364f8a
leduclinh-it <leduclinh65@gmail.com>
2020-10-29 22:58:18 +0700

    test deploy

--- src/main/java/dev/leduclinh/cdbusiness/repositories/CategoryRepository.java
+++ src/main/java/dev/leduclinh/cdbusiness/repositories/CategoryRepository.java
@@ -2,6 +2,8 @@ package dev.leduclinh.cdbusiness.repositories;
 
 import dev.leduclinh.cdbusiness.domain.entities.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 }
